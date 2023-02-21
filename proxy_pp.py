c032204f6ee48d3c35f424299f9458fb630dc18e
MrBunto <Buntoher@gmail.com>
2023-02-21 23:32:37 +0300

    Adding proxy parcer

---
+++ proxy_pp.py
@@ -0,0 +1,35 @@
+import scrapy
+from scrapy.http import HtmlResponse
+from scrapy import Selector
+import requests
+from scrapy.crawler import CrawlerProcess
+
+
+
+class ProxyList(scrapy.Spider):
+    name = "proxy"
+    allowed_domains = ["free-proxy-list.net"]
+    start_urls = ['https://free-proxy-list.net/']
+
+    def parse(self, response):
+        # item = ProxyItem()
+
+        table_rows = response.xpath(".//*[@class='table table-striped table-bordered']/"
+                                    "tbody/tr")
+
+        for row in table_rows:
+            yield {'ip': row.xpath('./td/text()').extract_first(),
+                   'port': row.xpath('./td[2]/text()').extract_first(),
+                   'last_checked' : row.xpath('./td[8]/text()').extract_first(),
+
+                   }
+from scrapy.crawler import CrawlerProcess
+process = CrawlerProcess(settings={
+    "FEEDS": {
+        "Output/output_proxy_list.csv": {"format": "csv"},
+    },
+})
+
+
+process.crawl(ProxyList)
+process.start()
